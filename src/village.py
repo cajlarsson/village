1eaa05b5a425f31b1832fb6313fe174d6a7463b6
Caj Larsson <caj.larsson@gmail.com>
2012-03-09 18:27:30 +0100

    gitignore and inspirational test

---
+++ src/village.py
@@ -0,0 +1,34 @@
+import time
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+width = 55
+height = 50
+side = 20
+screen = pygame.display.set_mode([width * side, height * side])
+
+pos = 0
+bild = pygame.image.load("../res/test-alien.png")
+bild = pygame.Surface.convert_alpha(bild)
+
+last = time.time()
+
+clock = pygame.time.Clock()
+while pygame.event.poll().type != KEYDOWN:
+
+
+    for x in range(width ):
+       for y in range(height):    
+           screen.blit(bild,[side * x , side * y] )
+
+    pygame.display.flip()
+    clock.tick()
+    
+    print(clock.get_fps())
+    
+    time.sleep(0.02)
+
+
+
