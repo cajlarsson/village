0c10fc366a96570a0572831cbf7133d1d07c3be3
Gustav Svensk <grulfen3@gmail.com>
2012-03-14 21:47:19 +0100

    Grulfen

--- src/village_pyglet.py
+++ src/village_pyglet.py
@@ -4,7 +4,7 @@ from pyglet.window import key
 clk = pyglet.clock.Clock()
 
 screen_width = 800
-screen_height = 600
+screen_height = 560
 
 window = pyglet.window.Window(screen_width, screen_height)
 
@@ -27,25 +27,34 @@ for i in range(screen_width_sprite*screen_height_sprite):
 	x = i % screen_width_sprite
 	grass_sprites.append(pyglet.sprite.Sprite(img=grass_image,x=x*sprite_size, y=y*sprite_size, batch=main_batch))
 
-player = pyglet.sprite.Sprite(img=player_image,x=400,y=300,batch=main_batch)
+player = pyglet.sprite.Sprite(img=player_image,x=0,y=100,batch=main_batch)
+
+fps_display = pyglet.clock.ClockDisplay(color=(1,0,0,1))
 
 @window.event
 def on_draw():
-	window.clear()
-	main_batch.draw()
+    clk.tick()
+    window.clear()
+    main_batch.draw()
+    fps_display.draw()
 
 
 @window.event
 def on_key_press(symbol,modifier):
 	if symbol == key.LEFT and player.x > 0:
 		player.set_position(player.x-20,player.y)
-	if symbol == key.UP and player.y < 580:
+	if symbol == key.UP and player.y < screen_height-sprite_size:
 		player.set_position(player.x,player.y+20)
-	if symbol == key.RIGHT and player.x < 780:
+	if symbol == key.RIGHT and player.x < screen_width-sprite_size:
 		player.set_position(player.x+20,player.y)
 	if symbol == key.DOWN and player.y > 0:
 		player.set_position(player.x,player.y-20)
 
+def move_right(dt):
+    if player.x < screen_width - sprite_size:
+        player.set_position(player.x+sprite_size,player.y)
+
+clk.schedule_interval(move_right, 1)
 
 if __name__ == '__main__':
 	pyglet.app.run()
