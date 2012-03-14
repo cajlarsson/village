10b7a09b0c12a859d10215e3c6e1640adc02ea10
Gustav Svensk <grulfen3@gmail.com>
2012-03-14 19:34:09 +0100

    Fixat village_pyglet

--- src/village_pyglet.py
+++ src/village_pyglet.py
@@ -1,3 +1,48 @@
+import pyglet
+
+screen_width = 800
+screen_height = 600
+
+window = pyglet.window.Window(screen_width, screen_height)
+
+sprite_size = 20
+
+main_batch = pyglet.graphics.Batch()
+
+screen_width_sprite = screen_width // sprite_size
+screen_height_sprite = screen_height // sprite_size
+
+pyglet.resource.path = ['../res']
+pyglet.resource.reindex()
+
+player_image = pyglet.resource.image('test-alien.png')
+grass_image = pyglet.resource.image('grass.png')
+
+grass_sprites = []
+for i in range(screen_width_sprite*screen_height_sprite):
+	y = i // screen_width_sprite
+	x = i % screen_width_sprite
+	grass_sprites.append(pyglet.sprite.Sprite(img=grass_image,x=x*sprite_size, y=y*sprite_size, batch=main_batch))
+
+player = pyglet.sprite.Sprite(img=player_image,x=400,y=300,batch=main_batch)
+
+@window.event
+def on_draw():
+	window.clear()
+	
+	main_batch.draw()
+
+
+@window.event
+def on_key_press(symbol,modifier):
+	if symbol == 65361 and player.x > 0:
+		player.set_position(player.x-20,player.y)
+	if symbol == 65362 and player.y < 580:
+		player.set_position(player.x,player.y+20)
+	if symbol == 65363 and player.x < 780:
+		player.set_position(player.x+20,player.y)
+	if symbol == 65364 and player.y > 0:
+		player.set_position(player.x,player.y-20)
 
 if __name__ == '__main__':
 	pyglet.app.run()
